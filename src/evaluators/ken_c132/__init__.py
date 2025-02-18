a22d61238e957567e76bfdec394ec201e3d5a5d9
Ken <ken.xiaoliang@gmail.com>
2025-02-18 11:40:45 +0000

    refactored module.run

--- src/evaluators/ken_c132/__init__.py
+++ src/evaluators/ken_c132/__init__.py
@@ -1,12 +1,1 @@
-def run(paper_content, gen_outputs_content):
-
-    # Your core code which runs some algo
-    # on the paper_content and gen_outputs_content
-    # and produces required
-    # outputs. 
-
-    outputs = {
-        "report": f"{paper_content} is good!"
-    }
-    
-    return outputs
+from .runner import run
