9f70acff5bf15cce419e69c7589904f85360c96b
Ken <ken.xiaoliang@gmail.com>
2025-02-18 11:39:10 +0000

    refactored module.run

--- src/generators/ken_c137/__init__.py
+++ src/generators/ken_c137/__init__.py
@@ -1,17 +1,1 @@
-def run(paper_content):
-
-    # Your core code which runs some algo
-    # on the paper_content and produces required
-    # outputs. 
-    
-    outputs = {
-        "doi": "",
-        "llm": "",
-        "methods": "",
-        "results": paper_content,
-        "knowledge_graph": "",
-        "knowledge_graph_permutations": {},
-        "results_permutations": {},
-        "num_permutations": 0
-    }
-    return outputs
+from .runner import run
---
+++ src/generators/ken_c137/runner.py
@@ -0,0 +1,17 @@
+def run(paper_content):
+
+    # Your core code which runs some algo
+    # on the paper_content and produces required
+    # outputs. 
+    
+    outputs = {
+        "doi": "",
+        "llm": "",
+        "methods": "",
+        "results": paper_content,
+        "knowledge_graph": "",
+        "knowledge_graph_permutations": {},
+        "results_permutations": {},
+        "num_permutations": 0
+    }
+    return outputs
